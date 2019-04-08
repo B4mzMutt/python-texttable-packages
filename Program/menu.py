ef83314ed315104684f519dd0e162d592dedce65
B4mzMutt <b4mzlavigne@gmail.com>
2019-04-08 22:55:45 +0700

    screen-02

--- Program/menu.py
+++ Program/menu.py
@@ -1,6 +1,6 @@
 def menu():
     from texttable import Texttable
     table = Texttable ()
-    table.add_rows([['NO','PILIHAN PROGRAM'],['1','Penilaian'],['2','Penggajian'],['3','Pembayaran'],['4','Calculator'],['5','Exit']])
+    table.add_rows([['NO','PILIHAN PROGRAM'],['1','Penggajian'],['2','Penilaian'],['3','Pembayaran'],['4','Calculator'],['5','Exit']])
     print (table.draw())
    
